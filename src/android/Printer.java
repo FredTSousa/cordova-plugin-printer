cd3a20cc3e0e846bde7ad106fc22a42d90dfeed0
FredTSousa <sousafred@gmail.com>
2025-09-10 14:16:59 +0100

    Update Printer.java

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -32,6 +32,7 @@ public class Printer extends CordovaPlugin {
             // Detect if content is base64
             if (content.startsWith("data:application/pdf;base64,")) {
                 content = content.replaceFirst("data:application/pdf;base64,", "");
+                Log.d("PrinterPlugin", "printBase64() called "+content);
                 printBase64(content, settings, callbackContext);
             } else {
                 print(content, settings, callbackContext);
@@ -52,13 +53,13 @@ public class Printer extends CordovaPlugin {
             FileOutputStream fos = new FileOutputStream(file);
             fos.write(data);
             fos.close();
-
+            Log.d("PrinterPlugin", "file wrote");
             Uri fileUri = FileProvider.getUriForFile(
                 cordova.getActivity(),
                 cordova.getActivity().getPackageName() + ".fileprovider",
                 file
             );
-
+            
             printJob(fileUri.toString(), settings, callbackContext);
         } catch (Exception e) {
             if (callbackContext != null) {
@@ -71,19 +72,22 @@ public class Printer extends CordovaPlugin {
 
     private void printJob(String url, JSONObject settings, CallbackContext callbackContext) {
         try {
+            Log.d("PrinterPlugin", "print job called "+url);
             Intent printIntent = new Intent(Intent.ACTION_VIEW);
             printIntent.setDataAndType(Uri.parse(url), "application/pdf");
             printIntent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
             cordova.getActivity().startActivity(printIntent);
-
+             Log.d("PrinterPlugin", "print job activity started ");
             if (callbackContext != null) {
                 PluginResult result = new PluginResult(PluginResult.Status.OK);
                 callbackContext.sendPluginResult(result);
             }
         } catch (Exception e) {
             if (callbackContext != null) {
+                Log.e("PrinterPlugin", "callback error: " + e.getMessage());
                 callbackContext.error("Failed to print document: " + e.getMessage());
+                
             } else {
                 Log.e(TAG, "Failed to print document: " + e.getMessage());
             }
